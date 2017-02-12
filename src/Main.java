32d1f5ed395c0fcab1a5d8886e274ed82c349ea8
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-02-12 19:42:25 +0300

    v1.5

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Util.maxNumber(123456789));
-        System.out.println(Util.maxNumber(987654321));
-        System.out.println(Util.maxNumber(123));
-        System.out.println(Util.maxNumber(695));
+        System.out.println(Util.factorial(5));
+        System.out.println(Util.maxNumber(923829348));
+        System.out.println(Util.sumNumber(123456789));
+        System.out.println(Util.minNumber(123456789));
+        System.out.println(Util.multiplicationNumber(123456789));
     }
 }
--- src/Util.java
+++ src/Util.java
@@ -19,4 +19,53 @@ public class Util {
         max = max>h? max:h; max = max>t? max:t;
         return max;
     }
+    public static int factorial(int n) {
+        int result;
+        if (n == 1) return 1;
+        result = factorial(n - 1) * n;
+        return result;
+    }
+    public static int minNumber(int number){    /**Не доделано, когда число меньше 9-ти знаков, выдает ноль, даже если его в числе нет.**/
+        int a, b, c, d, e , f, g, h, t;
+        int min;
+        a = number/100000000;
+        b = number/10000000%10;
+        c = number/1000000%10;
+        d = number/100000%10;
+        e = number/10000%10;
+        f = number/1000%10;
+        g = number/100%10;
+        h = number/10%10;
+        t = number%10;
+        min = a<b? a:b; min = min<c? min:c; min = min<d? min:d;
+        min = min<e? min:e; min = min<f? min:f; min = min<g? min:g;
+        min = min<h? min:h; min = min<t? min:t;
+        return min;
+    }
+    public static int sumNumber(int number){
+        int a, b, c, d, e , f, g, h, t;
+        a = number/100000000;
+        b = number/10000000%10;
+        c = number/1000000%10;
+        d = number/100000%10;
+        e = number/10000%10;
+        f = number/1000%10;
+        g = number/100%10;
+        h = number/10%10;
+        t = number%10;
+        return (a+b+c+d+e+f+g+h+t);
+    }
+    public static int multiplicationNumber(int number){
+        int a, b, c, d, e , f, g, h, t;      /**Не доделано, надо убрать из расчета ноль**/
+        a = number/100000000;
+        b = number/10000000%10;
+        c = number/1000000%10;
+        d = number/100000%10;
+        e = number/10000%10;
+        f = number/1000%10;
+        g = number/100%10;
+        h = number/10%10;
+        t = number%10;
+        return (a*b*c*d*e*f*g*h*t);
+    }
 }

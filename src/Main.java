7a17334af9be29f5ecf7632be2d1632802dbf10c
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-02-27 22:26:45 +0300

    666

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(Util.factorial(5));
-        System.out.println(Util.maxNumber(923829348));
+        System.out.println(Util.maxNumber(11156789));
         System.out.println(Util.sumNumber(123456789));
-        System.out.println(Util.minNumber(123456789));
-        System.out.println(Util.multiplicationNumber(1241241));
+        System.out.println(Util.minNumber(132453495));
+        System.out.println(Util.multiplicationNumber(1452));
 
     }
 }
--- src/Util.java
+++ src/Util.java
@@ -3,20 +3,11 @@
  */
 public class Util {
     public static int maxNumber(int number) {
-        int a, b, c, d, e , f, g, h, t;
-        int max;
-        a = number/100000000;
-        b = number/10000000%10;
-        c = number/1000000%10;
-        d = number/100000%10;
-        e = number/10000%10;
-        f = number/1000%10;
-        g = number/100%10;
-        h = number/10%10;
-        t = number%10;
-        max = a>b? a:b; max = max>c? max:c; max = max>d? max:d;
-        max = max>e? max:e; max = max>f? max:f; max = max>g? max:g;
-        max = max>h? max:h; max = max>t? max:t;
+        int max = -1;
+        do {
+            max = number%10>max? number%10:max;
+            number/=10;
+        } while (number>0);
         return max;
     }
 
@@ -27,49 +18,30 @@ public class Util {
         return result;
     }
 
-    public static int minNumber(int number){    /**Не доделано, когда число меньше 9-ти знаков, выдает ноль, даже если его в числе нет.**/
-        int a, b, c, d, e , f, g, h, t;
-        int min;
-        a = number/100000000;
-        b = number/10000000%10;
-        c = number/1000000%10;
-        d = number/100000%10;
-        e = number/10000%10;
-        f = number/1000%10;
-        g = number/100%10;
-        h = number/10%10;
-        t = number%10;
-        min = a<b? a:b; min = min<c? min:c; min = min<d? min:d;
-        min = min<e? min:e; min = min<f? min:f; min = min<g? min:g;
-        min = min<h? min:h; min = min<t? min:t;
+    public static int minNumber(int number){
+        int min = 10;
+        while (number>0) {
+            if (number%10<min) min = number%10;
+            number /= 10;
+        }
         return min;
     }
     public static int sumNumber(int number){
-        int a, b, c, d, e , f, g, h, t;
-        a = number/100000000;
-        b = number/10000000%10;
-        c = number/1000000%10;
-        d = number/100000%10;
-        e = number/10000%10;
-        f = number/1000%10;
-        g = number/100%10;
-        h = number/10%10;
-        t = number%10;
-        return (a+b+c+d+e+f+g+h+t);
+        int sum = 0;
+        while (number>0) {
+            sum = number%10 + sum;
+            number /= 10;
+        }
+        return sum;
     }
 
     public static int multiplicationNumber(int number){
-        int a, b, c, d, e , f, g, h, t;
-        a = number/100000000; if (a == 0) a = 1;
-        b = number/10000000%10; if (b == 0) b = 1;
-        c = number/1000000%10; if (c == 0) c = 1;
-        d = number/100000%10; if (d == 0) d = 1;
-        e = number/10000%10; if (e == 0) e = 1;
-        f = number/1000%10; if (f == 0) f = 1;
-        g = number/100%10; if (g == 0) g = 1;
-        h = number/10%10; if (h == 0) h = 1;
-        t = number%10; if (t == 0) t = 1;
-        return (a*b*c*d*e*f*g*h*t);
+        int mult = 1;
+        while (number>0) {
+            if (mult!=0) mult = number%10 * mult;
+            number /= 10;
+        }
+        return mult;
     }
 
     public static boolean searchNumeral(int number, int n){
